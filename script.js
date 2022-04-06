f8f99e26c641bf5e48af7994fc7daeb9faa0e8fb
Speez <89994772+Speez24@users.noreply.github.com>
2022-04-05 21:59:00 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.querySelector('button').onclick = myFunction;
 
 function myFunction() {
     const video = document.createElement('video');
-    video.src = 'videoplayback.mp4';
+    video.src = 'https://github.com/Speez24/totally-good-website/blob/main/videoplayback.mp4';
     video.autoplay = true;
     document.body.appendChild(video);
     console.log('Success!');
